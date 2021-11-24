e6c2c1644d4a928fbf122f81a1c0477f6142c61b
DavidDSS <daviddss1999@gmail.com>
2021-11-24 10:51:12 -0500

    Added Toolbar

--- app/src/main/java/com/example/meetingsapp/MainActivity.java
+++ app/src/main/java/com/example/meetingsapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.meetingsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Set Toolbar
+        Toolbar appBar = (Toolbar) findViewById(R.id.meetingsToolBar);
+        appBar.setBackgroundColor(getResources().getColor(R.color.design_default_color_primary));
+        appBar.setTitleTextColor(getResources().getColor(R.color.white));
+        setSupportActionBar(appBar);
+
+        //Initialize "Add Meeting" Btn
+        Button addMeetingBtn= findViewById(R.id.addMeetingBtn);
+        addMeetingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
     }
 }
