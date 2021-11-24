e3d1e926851bdba1a8a9383f0cee2507071f81ca
DavidDSS <daviddss1999@gmail.com>
2021-11-24 11:19:02 -0500

    Added files and logic for popup window display Added base file for meeting object

--- app/src/main/java/com/example/meetingsapp/MainActivity.java
+++ app/src/main/java/com/example/meetingsapp/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,8 +31,22 @@ public class MainActivity extends AppCompatActivity {
         addMeetingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                View popView = inflater.inflate(R.layout.add_meeting_window, null);
+
+                PopupWindow addMeetingPopUp = new PopupWindow(popView, LinearLayout.LayoutParams.MATCH_PARENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT, true);
+
+                addMeetingPopUp.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+                popView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        addMeetingPopUp.dismiss();
+                        return true;
+                    }
+                });
             }
         });
-
     }
 }
---
+++ app/src/main/java/com/example/meetingsapp/Meeting.java
@@ -0,0 +1,39 @@
+package com.example.meetingsapp;
+
+import java.util.Date;
+
+public class Meeting {
+    String title;
+    String location;
+    Date date;
+
+    public Meeting(String title, String location, Date date) {
+        this.title = title;
+        this.location = location;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
